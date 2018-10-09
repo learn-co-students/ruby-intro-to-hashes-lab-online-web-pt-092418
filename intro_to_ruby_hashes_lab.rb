26b8f00519292e836ddc56f0ceed21e3c051be37
Kevin J Gomez <kjgmez1@gmail.com>
2018-10-09 02:47:23 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,27 +1,87 @@
+require 'pry'
 def new_hash
-
+	Hash.new
 end
 
 def actor
-
+	actor = {name: "Dwayne The Rock Johnson"}
 end
 
 def monopoly
 	monopoly = {}
-
+	monopoly[:railroads] = {}
+	monopoly
 end
 
 def monopoly_with_second_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-
+	monopoly = {}
+	monopoly[:railroads] = {}
+	monopoly[:railroads][:pieces] = 4
+	monopoly
+	monopoly[:railroads][:names] = {}
+	monopoly
+	monopoly[:railroads][:rent_in_dollars] = {}
+	monopoly
 end
 
 def monopoly_with_third_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-
+	monopoly = {}
+	monopoly[:railroads] = {}
+	monopoly[:railroads][:pieces] = 4
+	monopoly
+	monopoly[:railroads][:names] = {}
+	monopoly
+	monopoly[:railroads][:rent_in_dollars] = {}
+	monopoly
+	monopoly[:railroads][:rent_in_dollars][:one_piece_owned] = 25
+	monopoly
+	monopoly[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
+	monopoly
+	monopoly[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
+	monopoly
+	monopoly[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
+	monopoly
+	monopoly[:railroads][:names][:reading_railroad] = {}
+	monopoly
+	monopoly[:railroads][:names][:pennsylvania_railroad] = {}
+	monopoly
+	monopoly[:railroads][:names][:b_and_o_railroad] = {}
+	monopoly
+	monopoly[:railroads][:names][:shortline] = {}
+	monopoly
 end
 
 def monopoly_with_fourth_tier
-	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-
+	monopoly = {}
+	monopoly[:railroads] = {}
+	monopoly[:railroads][:pieces] = 4
+	monopoly
+	monopoly[:railroads][:names] = {}
+	monopoly
+	monopoly[:railroads][:rent_in_dollars] = {}
+	monopoly
+	monopoly[:railroads][:rent_in_dollars][:one_piece_owned] = 25
+	monopoly
+	monopoly[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
+	monopoly
+	monopoly[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
+	monopoly
+	monopoly[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
+	monopoly
+	monopoly[:railroads][:names][:reading_railroad] = {}
+	monopoly
+	monopoly[:railroads][:names][:pennsylvania_railroad] = {}
+	monopoly
+	monopoly[:railroads][:names][:b_and_o_railroad] = {}
+	monopoly
+	monopoly[:railroads][:names][:shortline] = {}
+	monopoly
+	monopoly[:railroads][:names][:reading_railroad]['mortgage_value']='$100'
+	monopoly
+	monopoly[:railroads][:names][:pennsylvania_railroad]['mortgage_value']='$200'
+	monopoly
+	monopoly[:railroads][:names][:b_and_o_railroad]['mortgage_value']='$400'
+	monopoly
+	monopoly[:railroads][:names][:shortline]['mortgage_value']='$800'
+	monopoly
 end
